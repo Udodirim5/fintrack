7dc08f0616db97f94cec7fb77f1e149e759be5c0
udodirim <udodirimwisdom@gmail.com>
2025-01-06 15:01:27 +0100

    feat: add process dependency, update Vite config for dynamic base path, and modify package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import process from 'process';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const basename = isProduction ? "/fintrack/" : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: basename,
+});
